743d71b5f707bfb737859f09adb6d84680ed0f23
owillard <oliviawillard04@gmail.com>
2025-10-28 20:29:08 -0700

    tweet dates completed

--- js/about.js
+++ js/about.js
@@ -12,13 +12,13 @@ function parseTweets(runkeeper_tweets) {
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	
-	// get number of tweets
+	// get distinct number of tweets
 	const numTweets = tweet_array.length;
 	document.getElementById("numberTweets").innerText = numTweets;
 
-	const dateList = tweet_array.map(tweet => new Date(tweet.time));
 
 	// find smallest and largest date
+	const dateList = tweet_array.map(tweet => new Date(tweet.time));
 	let earliestDate = dateList[0];
 	let latestDate = dateList[0];
 
@@ -31,15 +31,14 @@ function parseTweets(runkeeper_tweets) {
 		}
 	}
 
-	const format = { weekday: "long", year: "numeric", month: "long", day: "numeric"};
-	const earliestText = earliestDate.toLocaleDateString("en-US", format);
-	const latestText = latestDate.toLocaleDateString("en-US", format);
+	const firstDate = earliestDate.toLocaleDateString("en-US", {dateStyle: "full"});
+	const lastDate = latestDate.toLocaleDateString("en-US", {dateStyle: "full"});
 
-	document.getElementById("firstDate").innerText = earliestText;
-	document.getElementById("lastDate").innerText = latestText;
+	document.getElementById("firstDate").innerText = firstDate;
+	document.getElementById("lastDate").innerText = lastDate;
 
-	console.log("Earliest tweet v2:", earliestText);
-	console.log("Latest tweet:", latestText);
+	console.log("Earliest tweet v2:", firstDate);
+	console.log("Latest tweet:", lastDate);
 
 	// count categories
 	let completed = 0, live = 0, achieve = 0, misc = 0;
